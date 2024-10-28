532e4b017196f70b2d59fc1d24a719e138bca29d
brewcold <seung.yoon.yu@gmail.com>
2024-10-28 15:58:12 +0900

    feat: add list

---
+++ src/react/list.tsx
@@ -0,0 +1,17 @@
+import { forwardRef, type ComponentPropsWithRef, type ElementType, type Ref } from "react"
+
+type ListProps<T extends ElementType> = {
+  as?: T
+} & ComponentPropsWithRef<T>
+
+function Component<T extends ElementType>({ as, ...props }: ListProps<T>, ref: Ref<Element>) {
+  const Comp = as || "li"
+
+  return (
+    <Comp ref={ref} {...props}>
+      {props.children}
+    </Comp>
+  )
+}
+
+export const List = forwardRef(Component)
