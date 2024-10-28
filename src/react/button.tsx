09c76ae59d1988a32afb600cc4bb7924d600c142
brewcold <seung.yoon.yu@gmail.com>
2024-10-28 15:58:03 +0900

    feat: add button

---
+++ src/react/button.tsx
@@ -0,0 +1,17 @@
+import { forwardRef, type ComponentPropsWithRef, type ElementType, type Ref } from "react"
+
+type BtnProps<T extends ElementType> = {
+  as?: T
+} & ComponentPropsWithRef<T>
+
+function Component<T extends ElementType>({ as, ...props }: BtnProps<T>, ref: Ref<Element>) {
+  const Comp = as || "button"
+
+  return (
+    <Comp ref={ref} {...props}>
+      {props.children}
+    </Comp>
+  )
+}
+
+export const Btn = forwardRef(Component)
