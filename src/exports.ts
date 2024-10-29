13725b881c2d698a43792ec2012fb09a271525ae
brewcold <seung.yoon.yu@gmail.com>
2024-10-29 17:57:49 +0900

    fix: modify exports

--- src/exports.ts
+++ src/exports.ts
@@ -1,3 +1,20 @@
-import { Example } from "./react/_template"
+import { Btn } from "./react/button"
+// import { Flex } from "./react/flex"
+// import { Form } from "./react/form"
+// import { Grid } from "./react/grid"
+// import {} from "./react/input"
+// import {} from "./react/label"
+import { List } from "./react/list"
+import { Spacing } from "./react/spacing"
+import { Txt } from "./react/txt"
+import { View } from "./react/view"
 
-export const SyComponent = Example
+const Bc = {
+  List,
+  Btn,
+  Spacing,
+  Txt,
+  View,
+}
+
+export default Bc
