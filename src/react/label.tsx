94841bc902ebc0497ec7dff525660b9d13090012
brewcold <seung.yoon.yu@gmail.com>
2024-11-01 17:50:34 +0900

    feat: add label

---
+++ src/react/label.tsx
@@ -0,0 +1,13 @@
+import { forwardRef, type ComponentPropsWithRef, type Ref } from "react"
+
+interface LabelProps extends ComponentPropsWithRef<"label"> {}
+
+function Component({ ...props }: LabelProps, ref: Ref<HTMLLabelElement>) {
+  return (
+    <label ref={ref} {...props}>
+      {props.children}
+    </label>
+  )
+}
+
+export const Label = forwardRef(Component)
